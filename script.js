5ec2e7603d1e3776200acfc95348641e2f5c6006
Brittney Cavazos <brittney.cavazos@gmail.com>
2021-03-23 17:25:35 -0700

    completed challenge four and formatted code

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
 /* ====== Coding Challenge: Javascript Arrays 3.24.21 ======
-4. Create a function that returns a duplicate free array
-  a. let duplicateArray = [1, 4, 9, 4, 100, 4, 30, 1]
-  b. the function should return [1, 4, 9, 100, 30] */
-
-
 
 /*1. Create a function that creates a new array with a given value
   a. The function should take two parameters: the size of the array and the item to pass in
@@ -22,6 +17,7 @@ duplication(10,'GO!');
 console.log(items)
 
 
+
 /* 2. Reverse: Write a function that reverses the order of the items in the array
   a. [1,2,3] = [3,2,1] */
 let bevies = ['Red Bull', 'Calipco', 'Chai Tea'];
@@ -35,6 +31,7 @@ const reverse = (array) =>{
 reverse(bevies);
 
 
+
 /* 3. Create a function that removes all unnecessary elements from an array
   a. use this array: let dirtyArray = [1, 'apple', false, null, 'bananas', 45, '', 'alkali', undefined];
   b. remove the falsy values so that the function returns: [1, 'apple', 'bananas', 45, 'alkali']; */
@@ -46,3 +43,23 @@ const truthy = (array) => {
 
 truthy(dirtyArray);
 console.log();
+
+
+
+/* 4. Create a function that returns a duplicate free array
+  a. let duplicateArray = [1, 4, 9, 4, 100, 4, 30, 1]
+  b. the function should return [1, 4, 9, 100, 30] */
+
+let duplicateArray = [1, 4, 9, 4, 100, 4, 30, 1]; // The array with dupes
+console.log('Array with duplicates: ', duplicateArray);
+
+let noDupeArray = []; //setting up a new array
+console.log('noDupeArray before: ', noDupeArray);
+
+duplicateArray.forEach((num) => { //for each element in duplicateArray (old), run a function with parameter of 'num'
+    if (!noDupeArray.includes(num)) { //if the new array doesnt include the element(parameter) from the old array
+        noDupeArray.push(num); //push element (parameter) into new array
+    }
+});
+
+console.log('noDupeArray after: ', noDupeArray); 
